d1f5b20333ebda70d55a287fd6c4765ef61575c3
Ayan Ahmad KCL 19002255 <ayan.ahmad@kcl.ac.uk>
2021-02-12 22:54:28 +0400

    Fetch External Resource

--- src/components/MainChunk.js
+++ src/components/MainChunk.js
@@ -4,10 +4,15 @@ import { Button } from '@material-ui/core';
 
 
 const MainChunk = () => {
+    function butClick() {
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(json => console.log(json));
+    }
     return(
         <div>
         <Box color="text.primary">
-            <Button variant="contained" color="secondary">
+            <Button onClick={butClick} variant="contained" color="secondary">
                 Secondary
             </Button>
         </Box>
